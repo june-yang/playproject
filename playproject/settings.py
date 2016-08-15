7927a01dd6fb2bfa586ebab160622399889e6c56
MaferYangPointJun <Maferyang.Jun@gmail.com>
2016-08-15 23:57:01 +0800

    db setting

--- playproject/settings.py
+++ playproject/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'playproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'play@project12#$.',
+        'USER': 'root',
+        'PASSWORD': 'pass',
+        'HOST': '127.0.0.1',
+        'PORT': '3600',
     }
 }
 
